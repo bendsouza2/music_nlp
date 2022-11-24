3ea6647b277ce042e3286cddf2628a6db1ca0a78
bendsouza <bendsouza2@gmail.com>
2022-11-24 16:54:12 +0000

    removed non lyrics

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from langdetect import detect
+import re
 
-pd.options.display.max_columns = 4
+pd.options.display.max_columns = 5
 
 # Reading in the data
 first = pd.read_csv('full_lyric_data')
@@ -16,6 +17,11 @@ print(empty)
 first['lang_code'] = first['lyrics'].apply(detect)
 languages = (first['lang_code'].unique())
 
-eng_only = first[first['lang_code'] == 'en']
+eng_only = first.drop(first[first['lang_code'] != 'en'].index)
+eng_only.drop('lang_code', axis=1, inplace=True)
 
-
+# Further cleaning
+# Removing all characters up until # the first ']' - typically includes translation options, 'Lyrics' and song title
+eng_only['lyrics'] = eng_only['lyrics'].apply(lambda x: re.sub(r'^.*?]', '', x))
+# Removing all characters within [] - typically includes [Verse x] or [Intro]/[Outro]
+eng_only['lyrics'] = eng_only['lyrics'].apply(lambda x: re.sub('[\(\[].*?[\)\]]', '', x))
